afa25610ef0953e5b6403140b9867a8f96e471fc
Alexandr Nikiforov <kefirnot@gmail.com>
2017-10-16 23:56:43 +0700

    sourcemaps

---
+++ src/component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export class Component extends React.Component {
+	render() {
+		var title = 'It works!!!';
+		return <div>{title}</div>;
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Component } from './component';
 
 ReactDOM.render(
-	<div>It works!!!</div>,
+	<Component></Component>,
 	document.getElementById('root')
 );
